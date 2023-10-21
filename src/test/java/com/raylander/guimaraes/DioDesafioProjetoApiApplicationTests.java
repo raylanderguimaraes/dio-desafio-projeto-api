51c653a8fb53215f64e9b46d1c80fbfbe148ed8b
Raylander <raylander.guimaraes@gmail.com>
2023-10-21 10:41:31 -0300

    Primeiro Commit

---
+++ src/test/java/com/raylander/guimaraes/DioDesafioProjetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raylander.guimaraes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioProjetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
